ca1d3c8f650048c900ba87327ede7d357b3785a5
SankalyaTharindi <sankalya.tharindi@gmail.com>
2025-10-28 11:56:05 +0530

    Notification number changed

--- src/components/shared/layout/Header.tsx
+++ src/components/shared/layout/Header.tsx
@@ -101,7 +101,7 @@ export default function Header({ onMenuClick }: HeaderProps) {
               onClick={() => setIsOpen(true)}
               className="relative p-1 text-gray-500 hover:text-gray-800 focus:outline-none"
             >
-              <span className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs font-semibold text-white">10</span>
+              <span className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs font-semibold text-white">3</span>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 00-5-5.917V5a2 2 0 00-4 0v.083A6 6 0 004 11v3.159c0 .538-.214 1.055-.595 1.436L2 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
               </svg>
